1aa0d2bb9fc2cba1706769db35bf7916bf63f1e2
Franco <frfantusatti@protonmail.com>
2026-03-31 04:59:41 -0300

    fix gettlazytext

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -81,6 +81,13 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
